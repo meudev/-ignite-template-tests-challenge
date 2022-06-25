2c249cc749c2dbc0a989a2bde85d916b22866661
unknown <adrian@meudev.com.br>
2022-06-25 17:34:33 -0300

    Desafio 02 - Testes de integração

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,29 @@
+import { app } from '../../../../app';
+import request from 'supertest';
+import createConnection from '../../../../database';
+import { Connection } from 'typeorm';
+
+let connection: Connection;
+describe('Create user controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: 'test',
+        email: 'test@mail.com',
+        password: '1234'
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
